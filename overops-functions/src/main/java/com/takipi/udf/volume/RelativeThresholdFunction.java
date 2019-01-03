d3aa9c4d6a4668697d26bdc897d8f9ecd6991acc
Dor Levi <dor.levi@overops.com>
2019-01-03 13:55:45 +0200

    Utilize centralized test util

--- overops-functions/src/main/java/com/takipi/udf/volume/RelativeThresholdFunction.java
+++ overops-functions/src/main/java/com/takipi/udf/volume/RelativeThresholdFunction.java
@@ -1,10 +1,6 @@
 package com.takipi.udf.volume;
 
-import com.google.gson.Gson;
-import com.takipi.api.client.ApiClient;
-import com.takipi.api.client.data.view.SummarizedView;
-import com.takipi.api.client.util.view.ViewUtil;
-import com.takipi.udf.ContextArgs;
+import com.takipi.udf.util.TestUtil;
 
 public class RelativeThresholdFunction extends ThresholdFunction {
 	public static String validateInput(String rawInput) {
@@ -19,29 +15,12 @@ public class RelativeThresholdFunction extends ThresholdFunction {
 	// RelativeThresholdFunction
 
 	public static void main(String[] args) {
-
-		if ((args == null) || (args.length < 3)) {
-			throw new IllegalArgumentException("args");
-		}
-
-		ContextArgs contextArgs = new ContextArgs();
-
-		contextArgs.apiHost = args[0];
-		contextArgs.apiKey = args[1];
-		contextArgs.serviceId = args[2];
-
-		ApiClient apiClient = contextArgs.apiClient();
-
-		SummarizedView view = ViewUtil.getServiceViewByName(apiClient, contextArgs.serviceId, "All Events");
-
-		contextArgs.viewId = view.id;
+		String rawContextArgs = TestUtil.getViewContextArgs(args, "All Events");
 
 		// example values
-
 		String[] sampleValues = new String[] { "relative_to=Method_Calls", "threshold=100", "rate=0.01",
 				"label=Anomaly", "minInterval=24h", };
 
-		String rawContextArgs = new Gson().toJson(contextArgs);
 		RelativeThresholdFunction.execute(rawContextArgs, String.join("\n", sampleValues));
 	}
 }
