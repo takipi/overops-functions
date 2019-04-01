a5032d4755f53abb4668e9293a325ddfbcede064
Dor Levi <dor.levi@overops.com>
2019-04-01 13:14:02 +0300

    Report anomaly via AlertUtil instead of own implementation

--- overops-functions/src/main/java/com/takipi/udf/volume/AnomalyUtil.java
+++ overops-functions/src/main/java/com/takipi/udf/volume/AnomalyUtil.java
@@ -14,16 +14,14 @@ import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
 import com.takipi.api.client.ApiClient;
 import com.takipi.api.client.data.event.Action;
-import com.takipi.api.client.request.alert.Anomaly;
-import com.takipi.api.client.request.alert.AnomalyAlertRequest;
 import com.takipi.api.client.request.event.BatchForceSnapshotsRequest;
 import com.takipi.api.client.request.event.EventActionsRequest;
 import com.takipi.api.client.request.label.BatchModifyLabelsRequest;
 import com.takipi.api.client.request.label.CreateLabelRequest;
 import com.takipi.api.client.result.EmptyResult;
-import com.takipi.api.client.result.GenericResult;
 import com.takipi.api.client.result.event.EventActionsResult;
 import com.takipi.api.client.result.event.EventResult;
+import com.takipi.api.client.util.alert.AlertUtil;
 import com.takipi.api.core.url.UrlClient.Response;
 import com.takipi.common.util.CollectionUtil;
 import com.takipi.udf.input.TimeInterval;
@@ -167,7 +165,7 @@ public class AnomalyUtil {
 
 		resetContributorSnapshots(apiClient, serviceId, contributors);
 
-		send(apiClient, serviceId, viewId, contributors, from, to, anomalyMessage);
+		AlertUtil.reportAnomaly(apiClient, serviceId, viewId, contributors, from, to, anomalyMessage);
 	}
 
 	private static void applyAnomalyLabel(ApiClient apiClient, String serviceId, String label,
@@ -216,42 +214,6 @@ public class AnomalyUtil {
 
 		if (reponse.isBadResponse()) {
 			System.err.println("Cannot reset snapshots, code: " + reponse.responseCode);
-		}
-	}
-
-	private static void send(ApiClient apiClient, String serviceId, String viewId, Collection<EventResult> events,
-			DateTime from, DateTime to, String desc) {
-		// Send anomaly message to integrations
-
-		Anomaly anomaly = Anomaly.create();
-
-		anomaly.addAnomalyPeriod(viewId, from.getMillis(), to.getMillis());
-
-		for (EventResult event : events) {
-			if ((event.stats != null) && (event.stats.hits > 0)) {
-				anomaly.addContributor(Integer.parseInt(event.id), event.stats.hits);
-			}
-		}
-
-		AnomalyAlertRequest anomalyAlertRequest = AnomalyAlertRequest.newBuilder().setServiceId(serviceId)
-				.setViewId(viewId).setFrom(from.toString()).setTo(to.toString()).setDesc(desc).setAnomaly(anomaly)
-				.build();
-
-		Response<GenericResult> anomalyAlertResponse = apiClient.post(anomalyAlertRequest);
-
-		if (anomalyAlertResponse.isBadResponse()) {
-			throw new IllegalStateException("Failed alerting on anomaly for view - " + viewId);
-		}
-
-		GenericResult alertResult = anomalyAlertResponse.data;
-
-		if (alertResult == null) {
-			throw new IllegalStateException("Failed getting anomaly alert result on view - " + viewId);
-		}
-
-		if (!alertResult.result) {
-			throw new IllegalStateException(
-					"Anomaly alert on view - " + viewId + " failed with - " + alertResult.message);
 		}
 	}
 }
